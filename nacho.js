fbf900458a9df498112c2585c289e5c24d181d0c
trodoblasto <trodoblasto@gmail.com>
2024-02-04 17:41:30 +0100

    Commit final de la ramaB

--- nacho.js
+++ nacho.js
@@ -1,4 +1,6 @@
 var ejecuta = function(){
     var inputValue = document.getElementById("textInput").value;
-console.log(">>>> " + inputValue);
+    console.log(">>>> " + inputValue);
+    document.getElementById("muestraText").innerHTML = inputValue;
+    
 }
