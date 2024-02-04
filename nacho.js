ff79a6a7a9ab65886f339ee2025130ca1bd200c6
trodoblasto <trodoblasto@gmail.com>
2024-02-04 19:44:34 +0100

    Agunos comentarios añadidos a nacho.js y estilos.css

--- nacho.js
+++ nacho.js
@@ -1,5 +1,6 @@
 var ejecuta = function(){
     var inputValue = document.getElementById("textInput").value;
+    //Comnetario d prueba
     console.log(">>>> " + inputValue);
     document.getElementById("muestraText").innerHTML = inputValue;
     
